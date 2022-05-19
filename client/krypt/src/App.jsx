ae257ad70039d44374e04bf7fa14a8b090b108f3
stepanianpat <stepanian86@gmail.com>
2022-05-19 00:04:30 -0700

    First Commit

---
+++ client/krypt/src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )      
+}
+
+export default App
