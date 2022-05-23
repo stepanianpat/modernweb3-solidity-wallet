12456611ee66fb3cb520921adc6b702957450fe7
stepanianpat <stepanian86@gmail.com>
2022-05-22 23:52:31 -0700

    Recent changes

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { Navbar, Welcome, Footer, Services, Transactions } from './components';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <div className='min-h-screen'>
+        <div className='gradient-bg-welcome'>
+          <Navbar />
+          <Welcome />
+        </div>
+        <Services />
+        <Transactions />
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+import logo from '../../images/logo.png';
+import { useState } from 'react';
+
+const NavbarItem = ({title, classProps}) => {
+  return (
+    <li className={`mx-4 cursor-pointer ${classProps}`}>
+      {title}
+    </li>
+  )
+}
+
+const Navbar = () => {
+
+  const [toggleMenu,  setToggleMenu] = useState(false)
+
+  return (
+    
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+      <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        <img src={logo} alt="logo" className="w-32 cursor-pointer"  />
+      </div>
+      <ul className="text-white md:flex hiddent list-none flex-row justify-between items-center">
+        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+          <NavbarItem key={item + index} title={item} />
+        ))}
+        <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full  cursor-pointer hover:bg-[#2546bd]'>
+          login
+        </li>
+      </ul>
+
+      <div className='flex relative'>
+          {toggleMenu 
+            ? <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => {
+              setToggleMenu(false)
+            }}/>
+            : <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => {
+              setToggleMenu(true)
+            }} />
+          }
+
+          {toggleMenu && (
+            <ul>
+              <li className="text-xl w-full my-2">
+                <AiOutlineClose onClick={() => setToggleMenu(flase)}/> 
+              </li>
+            </ul>
+          )}
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
