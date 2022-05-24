873f770b8f4eafbf85f90dd0b8a2e06e71832ca0
stepanianpat <stepanian86@gmail.com>
2022-05-23 20:51:38 -0700

    Test push

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
         <img src={logo} alt="logo" className="w-32 cursor-pointer"  />
       </div>
-      <ul className="text-white md:flex hiddent list-none flex-row justify-between items-center">
+      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center">
         {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
           <NavbarItem key={item + index} title={item} />
         ))}
@@ -42,10 +42,15 @@ const Navbar = () => {
           }
 
           {toggleMenu && (
-            <ul>
+            <ul
+              className="z-10 fixed top-0 -right-2 p-3 w-[70vw] hscreen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
+            >
               <li className="text-xl w-full my-2">
-                <AiOutlineClose onClick={() => setToggleMenu(flase)}/> 
+                <AiOutlineClose onClick={() => setToggleMenu(false)} /> 
               </li>
+              {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+                <NavbarItem key={item + index} title={item} classProps="my-2 text-lg"/>
+              ))}
             </ul>
           )}
       </div>
