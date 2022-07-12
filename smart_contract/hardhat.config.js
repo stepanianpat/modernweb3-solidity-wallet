1e7378805cd4487864b1da9554e730c6778df465
stepanianpat <stepanian86@gmail.com>
2022-07-12 00:26:35 -0700

    Alch

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     rinkeby: {
       url: 'https://eth-rinkeby.alchemyapi.io/v2/v5yAQyoxWdn7tuY17cu_8XukYLJnrNMR',
-      accounts: [ '4759a9c1a791df714f95cf1597cc72cf448dd50b0e8c42ffec0381019f5dd8d2' ]
+      accounts: [ '' ]
     }
   }
 }
