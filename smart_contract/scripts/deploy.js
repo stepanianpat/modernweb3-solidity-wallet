2b536cff97692ef4e1e5d6755c5dd922f6992c69
stepanianpat <stepanian86@gmail.com>
2022-07-12 00:14:30 -0700

    Smart Contract Deployment

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -2,7 +2,7 @@ const main = async () => {
 
 
   const Transactions = await hre.ethers.getContractFactory("Transactions");
-  const transactions = await Transactions.deploy("Hello, Hardhat!");
+  const transactions = await Transactions.deploy();
 
   await transactions.deployed();
 
