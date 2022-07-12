e0daf9c7b1febfa9a4022dc60fb705381b43f7c4
stepanianpat <stepanian86@gmail.com>
2022-07-12 00:20:32 -0700

    Readme

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './Transactions.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = '';
+export const contractAddress = '0x457ac38ac145C0b86B71bb4e95d4C6A4ba81D517';
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/replace',
-      accounts: [ 'replace' ]
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/v5yAQyoxWdn7tuY17cu_8XukYLJnrNMR',
+      accounts: [ '4759a9c1a791df714f95cf1597cc72cf448dd50b0e8c42ffec0381019f5dd8d2' ]
     }
   }
 }
