74b14cdb569a4188290c5a4f7bef6b6df80ac9d7
stepanianpat <stepanian86@gmail.com>
2022-08-15 22:27:29 -0700

    Lots of changes to transactions, UI, metamask addition, etc.

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './Transactions.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = 'replace';
+export const contractAddress = '0x3D0bB1264ccD0F63c6c65555904147c063f8ff1e';
